35e00d9b637d22cc4959d7bb837df983fa3430b2
pavzel <Pavel.V.Zelenin@gmail.com>
2020-06-05 10:23:26 +0000

    A version without import env.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import env as config
 
 app = Flask(__name__)
 
@@ -81,4 +80,4 @@ def delete_place(place_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=os.environ.get('PORT'),
-            debug=True)
+            debug=False)
